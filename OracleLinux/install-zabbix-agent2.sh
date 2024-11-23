7c21c47dbe9f21a738fc9c8c28ffb04a86cd045d
Lukas Maly <smejdil@gmail.com>
2024-11-23 14:01:50 +0100

    Add OracleLinux VM with ZBX reg

---
+++ OracleLinux/install-zabbix-agent2.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Aktualizace OS
+#sudo dnf update -y
+
+# Stažení balíčku pro instalaci zabbix repo
+sudo rpm -Uvh https://repo.zabbix.com/zabbix/6.0/rhel/9/x86_64/zabbix-release-latest.el9.noarch.rpm
+sudo dnf clean all 
+
+# Instalace zabbix agent2
+sudo dnf install -y zabbix-agent2 zabbix-agent2-plugin-* 
+
+# Povoleni sluzby zabbix-agent2
+sudo systemctl enable zabbix-agent2
+
+# Restart sluzby zabbix-agent2
+sudo systemctl restart zabbix-agent2
+
+# EOF
